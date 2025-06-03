c14c9785461efba809835db62178a96c67ca05ed
Dipendra <hehesuiii69@gmail.com>
2025-06-03 14:54:28 +0545

    service locator

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:my_app_flutter/app.dart';
+import 'package:my_app_flutter/serviceLocator/service_locator.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await initDependenices();
   runApp(const App());
 }
 
